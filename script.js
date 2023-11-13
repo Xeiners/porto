eaf2bc429dde0cd635a5044ba6c7ed6a338d7a6e
Xeiners <xreizser@gmail.com>
2023-11-13 13:44:38 +0100

    NavBar Mobile

--- script.js
+++ script.js
@@ -21,6 +21,28 @@ document.querySelectorAll('.unReveal').forEach(function (r) {
 })
 
 
+const menus = document.querySelector('.menu');
+const liens = document.querySelector('.links');
+const btnDrop = document.querySelector('.btn-top');
+const listes = document.querySelectorAll('.menu li');
+
+let toggleIndex;
+
+btnDrop.addEventListener('click', toggleDropDown);
+
+function toggleDropDown() {
+    if (!toggleIndex) {
+        liens.style.height = `${liens.scrollHeight}px`;
+        liens.style.margin=  '0px 0px 0px 0px';
+        toggleIndex = true;
+        return;
+    }
+
+    liens.style.margin=  '0px -150px 0px 0px';
+    liens.style.height = 0;
+    toggleIndex = false;
+}
+
 // window.onload = function () {
 //     window.scrollTo(0, 0);
 //   }
